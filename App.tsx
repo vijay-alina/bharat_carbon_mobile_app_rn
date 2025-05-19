689799df140fc4376fca52c3d61b6736172ac1a4
lalit-vats <117825355+lalit-vats@users.noreply.github.com>
2025-05-19 14:34:49 +0530

    Fix conflicts issues again

--- App.tsx
+++ App.tsx
@@ -7,95 +7,12 @@
 import 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
-<<<<<<< HEAD
-import type {PropsWithChildren} from 'react';
-import {
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-import HomeVerticalCard from './src/home-horizontal-card';
-import HomeHorizontalCard from './src/home-horizontal-card';
-import VerticalClimateCard from './src/home-vertical-card';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-=======
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import { DrawerNavigator } from './src/navigations/drawerNavigator';
->>>>>>> 6cbd5ad8a130d4235e0863f64f770f0d9312e72a
 
 function App(): React.JSX.Element {
   return (
-<<<<<<< HEAD
-    <View style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        style={backgroundStyle}>
-        <View style={{paddingRight: safePadding}}>
-         {/* <Header/>*/}
-        </View>
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-            paddingHorizontal: safePadding,
-            paddingBottom: safePadding,
-          }}>
-        
-        
-          <HomeHorizontalCard />
-          <VerticalClimateCard
-            imageUri="https://example.com/image.jpg"
-            title="Climate Action"
-            subtitle="Take steps to reduce your carbon footprint"
-            buttonText="Upload Now"
-          />
-        </View>
-      </ScrollView>
-    </View>
-=======
     <GestureHandlerRootView style={{flex: 1}}>
       <SafeAreaProvider>
         <NavigationContainer>
@@ -103,7 +20,6 @@ function App(): React.JSX.Element {
         </NavigationContainer>
       </SafeAreaProvider>
     </GestureHandlerRootView>
->>>>>>> 6cbd5ad8a130d4235e0863f64f770f0d9312e72a
   );
 }
 
